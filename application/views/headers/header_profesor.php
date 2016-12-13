1186ee843b55c974946d4ac6dd912e8c7d8a3e0e
hectoriibio <hector.carrs@gmail.com>
2016-12-13 20:39:50 -0300

    aaa

--- application/views/headers/header_profesor.php
+++ application/views/headers/header_profesor.php
@@ -9,7 +9,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="">Profesor</a>
+                <a class="navbar-brand" href="<?php echo base_url();?>index.php/profesor">Profesor</a>
             </div>
             <!-- Top Menu Items -->
             <ul class="nav navbar-right top-nav">
